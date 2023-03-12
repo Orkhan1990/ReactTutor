725fb045fb74661f8c0a8ce04af8fd9cc225f2e4
Orkhan Talibli <orkhan.talibli@gmail.com>
2023-03-12 23:33:44 +0400

    second commit

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import './loginPage.css';
 
-export default class LoginPage extends Component {
+ class LoginPage extends Component {
   render() {
     return (
-      <div>LoginPage</div>
+      <div className='login_page_container'>
+         <h2>Login</h2>
+         <form >
+          <div className='login_input'>
+          <label htmlFor="email">Email</label>
+          <input id='email' type="text" placeholder='Email' />
+          </div>
+
+          <div className='login_input'>
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder='Password' id="password"/>
+          </div>
+            <Link className='login_forgot_password'>Forgot Password?</Link>
+            <p></p>
+            <button type='submit' className='login_page_btn'>Login</button>
+         </form>
+      </div>
     )
   }
 }
+
+export default LoginPage;
